b721bb82998a123ada97ca60d3721cdf5fcf8a87
Aaron Renner <aaron.renner@gmail.com>
2018-05-25 22:12:41 -0600

    Add Travis, credo and dialyzer

--- mix.exs
+++ mix.exs
@@ -31,6 +31,8 @@ defmodule Constantizer.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:credo, "~> 0.9.1", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 0.5", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.16", only: :dev, runtime: false}
     ]
   end
--- test/constantizer_test.exs
+++ test/constantizer_test.exs
@@ -1,3 +1,4 @@
+# credo:disable-for-this-file Credo.Check.Design.AliasUsage
 defmodule ConstantizerTest do
   use ExUnit.Case
 
