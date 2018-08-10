2ee8017c7d5a1cb5fd456abdbfbebd399b872dca
Aaron Renner <aaron.renner@gmail.com>
2018-08-09 23:49:13 -0600

    Upgrade credo, dialyxir and check format on travis

--- mix.exs
+++ mix.exs
@@ -31,8 +31,8 @@ defmodule Constantizer.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 0.9.1", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 0.5", only: [:dev, :test], runtime: false},
+      {:credo, "~> 0.10", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.3", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.16", only: :dev, runtime: false}
     ]
   end
